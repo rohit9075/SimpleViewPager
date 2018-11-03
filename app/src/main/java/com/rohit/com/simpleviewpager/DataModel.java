32f23aec16f6cd2dbd9a90eb7d2bc6cb36a1125e
Rohit Kumar <rohit9075@gmail.com>
2018-11-03 09:26:54 +0530

    Initial commit

---
+++ app/src/main/java/com/rohit/com/simpleviewpager/DataModel.java
@@ -0,0 +1,12 @@
+package com.rohit.com.simpleviewpager;
+
+public class DataModel {
+
+    private String imageId;
+    private String title;
+
+    public DataModel(String imageId, String title) {
+        this.imageId = imageId;
+        this.title = title;
+    }
+}
